2be51b84bb3b9dcd340ec649be41d0ab06003bfc
Lee Ji Yoon <leejiyooniiiii@gmail.com>
2025-07-24 09:24:34 +0900

    seperate CSS module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import headerModuleCss from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </header>
+    <>
+      <p className={headerModuleCss.paragraph}> waiting for you.</p>
+      <header>
+        <img src={logo} alt="A canvas" />
+        <h1>ReactArt</h1>
+        <p>A community of artists and art-lovers.</p>
+      </header>
+    </>
   );
 }
